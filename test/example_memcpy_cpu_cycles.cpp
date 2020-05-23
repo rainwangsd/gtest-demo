42f44470eaf559b124db2c04b4f9b570c62a532a
zwang <rain.wang.szcn@gmail.com>
2020-05-23 16:25:22 -0700

    update for different memcpy size

--- test/example_memcpy_cpu_cycles.cpp
+++ test/example_memcpy_cpu_cycles.cpp
@@ -1,17 +1,16 @@
 #include "gtest/gtest.h"
 #include "example.h"
 
-// #include <iostream>
 #include <new>
 #include <stdio.h>
-// using namespace std
 
-TEST(example, memcpy_cpu_cycles_1500)
+int memcpy_cpu_time(int mem_size)
 {
-    double res;
-    res = add_numbers(1.0, 2.0);
-    ASSERT_NEAR(res, 3.0, 1.0e-11);
-    const uint32_t mem_size = 1518; // 1 * 1024 * 1024;
+    // double res;
+    // res = add_numbers(1.0, 2.0);
+    // ASSERT_NEAR(res, 3.0, 1.0e-11);
+
+    // const uint32_t mem_size = 1518; // 1 * 1024 * 1024;
 
     char * buf_src = new char[mem_size];
     char * buf_dst = new char[mem_size];
@@ -22,24 +21,49 @@ TEST(example, memcpy_cpu_cycles_1500)
     
     delete [] buf_dst;
 
+    return 0;
+}
+
+TEST(example, memcpy_cpu_cycles_1500)
+{
+    const uint32_t mem_size = 1518; // 1 * 1024 * 1024;
+
+    memcpy_cpu_time(mem_size);
+}
+
+TEST(example, memcpy_cpu_cycles_32kb)
+{
+    const uint32_t mem_size = 32 * 1024;
+
+    memcpy_cpu_time(mem_size);
+}
+
+TEST(example, memcpy_cpu_cycles_128kb)
+{
+    const uint32_t mem_size = 128 * 1024;
+
+    memcpy_cpu_time(mem_size);
+}
+
+TEST(example, memcpy_cpu_cycles_256kb)
+{
+    const uint32_t mem_size = 256 * 1024;
+
+    memcpy_cpu_time(mem_size);
 }
 
 TEST(example, memcpy_cpu_cycles_1m)
 {
-    double res;
-    res = add_numbers(1.0, 2.0);
-    ASSERT_NEAR(res, 3.0, 1.0e-11);
     const uint32_t mem_size = 1 * 1024 * 1024;
 
-    char * buf_src = new char[mem_size];
-    char * buf_dst = new char[mem_size];
+    memcpy_cpu_time(mem_size);
+}
 
-    memcpy(buf_dst, buf_src, mem_size);
+TEST(example, memcpy_cpu_cycles_6m_call_sub)
+{
+    const uint32_t mem_size = 6 * 1024 * 1024;
 
-    delete [] buf_src;
-    
-    delete [] buf_dst;
-
+    memcpy_cpu_time(mem_size);
 }
 
 TEST(example, memcpy_cpu_cycles_6m)
@@ -57,5 +81,4 @@ TEST(example, memcpy_cpu_cycles_6m)
     delete [] buf_src;
     
     delete [] buf_dst;
-
 }
