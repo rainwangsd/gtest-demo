a146f9989a2c30f8332091111748db6c23017c78
zwang <rain.wang.szcn@gmail.com>
2020-07-05 11:03:56 -0700

    gtests function generation.

--- test/example_run_tests.cpp
+++ test/example_run_tests.cpp
@@ -124,30 +124,40 @@ std::string wled_tests[] = {
 //   return ret;
 // }
 
+#define DEBUG          1
+
+#if DEBUG 
+#define INFO(msg) \
+    fprintf(stderr, "INFO: %s,%s(),L%d: ", __FILE__, __FUNCTION__, __LINE__); \
+    fprintf(stderr, "%s\n", msg);
+#else
+#define INFO(msg)
+#endif
+
 // Read tests from files
 int tests_run_tdd_get_cases(string tests_file, vector<string> &tests)
 {
   int ret = -1;
 
-  const char *tst_help = " wled tests all ";
+  const char *tst_help = " tests_run_tdd_get_cases ";
   cout << tst_help << endl;
+  INFO("ENTRY");
 
   string tst_cmd = "cd ../tdd/tools/tests/wled; ";
   tst_cmd = tst_cmd + "tdd_t_maint_v8 ";
 
   // TODO: FIXME, to add tests from config file
   // Declaring vector of string type for tests
-//   vector<string> tests;
+  // vector<string> tests;
   int tests_num = 0;
 
   ifstream input;
-//   string tests_file = "../tdd/tools/tests/wled/wled_tests.sh";
   system("pwd");
   input.open(tests_file);
   if (input.fail()) {
     cout << "Error: no tests file existing!(" << tests_file << ")" << endl;
     // return ret;
-  } else {    
+  } else {
     string line_str;
     string token;
 
@@ -157,10 +167,10 @@ int tests_run_tdd_get_cases(string tests_file, vector<string> &tests)
       stringstream in_line(line_str);
       int token_num = 0;
       while (in_line >> token) {
-        //   cout << " here is a test: " << token << endl;
+        // cout << " here is a test: " << token << endl;
         // Check comment or test, #
         char test_flag = token.at(0);
-        //   cout << "test flag: " << test_flag << endl;
+        // cout << "test flag: " << test_flag << endl;
         if (test_flag == '#' && token_num == 0) {
           line_comment_flag = 1;
           break;
@@ -175,12 +185,11 @@ int tests_run_tdd_get_cases(string tests_file, vector<string> &tests)
       if (line_comment_flag == 1) {
         continue; // skip the line with
       }
-
     }  // line_str
   }
 
   cout << "### There are " << tests_num << " total tests." << endl;
-  // run tests
+  // display tests
   for (int i = 0; i < tests.size(); i++) {
     // Convert string to const char * for system call
     string start_cmd = tst_cmd;
@@ -191,9 +200,77 @@ int tests_run_tdd_get_cases(string tests_file, vector<string> &tests)
     // cout << "ret: " << ret << endl;
   }
 
-  // generate cpp test file for 
-
+  INFO("EXIT");
   return ret;
+}
+
+// generate test cpp code
+int tests_generate_code(string tst_cmd, vector<string> tests)
+{
+  string file_ouput = "./test/tests_wled.cpp";
+  ofstream output;
+  output.open(file_ouput);
+
+  if (output.fail()) {
+    cout << "Error to create tests file !(" << file_ouput << ")" << endl;
+  } else {
+    // Header and comment
+    string str = "/* tests - wled, dynamic generated file from tool */ \n";
+    // str += '\n';
+    str += "#include \"gtest/gtest.h\" \n";
+    str += "#include \"example.h\" \n";
+    str += "\n";
+    str += "#include <new> \n";
+    str += "#include <stdio.h> \n";
+    str += "\n";
+    str += "#include <bits/stdc++.h> \n\n";
+    str += "using namespace std; \n";
+    output << str << endl;
+
+    // TEST(example, tests_run_tdd_wled_all)
+    // {
+    //   int ret = 0;
+    //   ret = tests_run_tdd_wled_all();
+    //   ASSERT_EQ(ret, 0);
+    //   // ASSERT_EQ(ret, 1);
+    // }
+
+    for (int i = 0; i < tests.size(); i++) {
+      string run_cmd = tst_cmd;
+      str = "TEST(example, ";
+      // Convert string to const char * for system call
+      string start_cmd;
+      start_cmd = tests[i].c_str();
+
+      // run_cmd += " ";
+      run_cmd += start_cmd;
+
+      const char *command = run_cmd.c_str();
+      cout << "cmd: " << command << endl;
+
+      str += start_cmd;
+      str += ") \n";
+      str += "{ \n";
+
+      str += "  int ret = 0; \n";
+      // str += "  ret = tests_run_tdd_wled_all(); \n";
+      str += "  const char *command = \"";
+      // test cmd
+      str += run_cmd.c_str();
+      str += " \" ; \n";
+      str += "  ret = system(command); \n";
+      str += "  cout << \"ret: \" << ret << endl; \n";
+      str += "  ASSERT_EQ(ret, 0); \n";
+      str += "  // ASSERT_EQ(ret, 1); \n";
+      str += "} \n";
+
+      // Write to cpp file
+      output << str << endl;
+    }
+
+    output.close();
+  }
+
 }
 
 // Read tests from files
@@ -210,50 +287,12 @@ int tests_run_tdd_wled_all()
   // TODO: FIXME, to add tests from config file
   // Declaring vector of string type for tests
   vector<string> tests;
-//   int tests_num = 0;
+  // int tests_num = 0;
 
   ifstream input;
   string tests_file = "../tdd/tools/tests/wled/wled_tests.sh";
   system("pwd");
   ret = tests_run_tdd_get_cases(tests_file, tests);
-
-//   input.open(tests_file);
-//   if (input.fail()) {
-//     cout << "Error: no tests file existing!(" << tests_file << ")" << endl;
-//     // return ret;
-//   } else {    
-//     string line_str;
-//     string token;
-
-//     // Initialize vector with string using push_back
-//     while (getline(input, line_str)) {
-//       int line_comment_flag = 0;
-//       stringstream in_line(line_str);
-//       int token_num = 0;
-//       while (in_line >> token) {
-//         //   cout << " here is a test: " << token << endl;
-//         // Check comment or test, #
-//         char test_flag = token.at(0);
-//         //   cout << "test flag: " << test_flag << endl;
-//         if (test_flag == '#' && token_num == 0) {
-//           line_comment_flag = 1;
-//           break;
-//         }
-//         cout << " here is a test: " << token << endl;
-//         tests.push_back(token);
-//         token_num++;
-//         tests_num++;
-//         // only handle 1st token
-//         break;
-//       }
-//       if (line_comment_flag == 1) {
-//         continue; // skip the line with
-//       }
-
-//     }  // line_str
-//   }
-
-//   cout << "### There are " << tests_num << " total tests." << endl;
 
   // run tests
   for (int i = 0; i < tests.size(); i++) {
@@ -266,7 +305,8 @@ int tests_run_tdd_wled_all()
     cout << "ret: " << ret << endl;
   }
 
-  // generate cpp test file for 
+  // generate cpp test file for (optional !!)
+  tests_generate_code(tst_cmd, tests);
 
   return ret;
 }
